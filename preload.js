c19f809766f1340017efe922a5d677c9f6900242
MCSilvers <chance.silvers1@gmail.com>
2025-07-18 12:43:56 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,13 @@
 // preload.js
 const os = require('os');
+const fs = require('fs');
+// Expose the hostname
 window.getHostname = () => os.hostname();
+
+// Expose the log appending function
+window.appendLog = (message) => {
+  const logPath = path.join(__dirname, 'calendar.log');
+  const timestamp = new Date().toISOString();
+  const logMessage = `[${timestamp}] ${message}\n`;
+  fs.appendFileSync(logPath, logMessage, 'utf8');
+};
